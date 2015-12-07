d5e2da6748fc2372c5ea2c00115444cbd17d07fd
Joah Gerstenberg <me@joahg.com>
2015-12-07 17:43:21 -0600

    dec slides

--- 2015/dec/script.js
+++ 2015/dec/script.js
@@ -15,15 +15,30 @@ var prevSlide = function() {
     window.location.hash = window.currentSlide.toString();
     return true;
   }
+
   return false;
 };
 
 window.onload = function() {
+  Array.prototype.forEach.call(document.querySelectorAll('.slide'), function(slide, i) {
+    slide.setAttribute('id', (i + 1).toString());
+  });
+
+  history.pushState({}, "", this.href);
+
   window.currentSlide = parseInt(window.location.hash.substr(1)) - 1 || 0;
   nextSlide();
+
+  document.getElementById(window.currentSlide).setAttribute('style', 'display: block;')
 };
 
 window.onkeydown = function(e) {
+  if ([33,34,37,39].indexOf(e.which) > -1) {
+    Array.prototype.forEach.call(document.querySelectorAll('.slide'), function(slide) {
+      slide.setAttribute('style', '');
+    });
+  }
+
   if (e.which === 39 || e.which === 34) {
     e.preventDefault();
     nextSlide();
